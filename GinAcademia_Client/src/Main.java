76fd6743ef95aa26aaec702173845cc87ace9c40
hieukut456 <trunghieu9946@gmail.com>
2020-12-01 15:57:07 +0700

    a

--- GinAcademia_Client/src/Main.java
+++ GinAcademia_Client/src/Main.java
@@ -12,8 +12,8 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }	
-		Login login = new Login("localhost",5000); 
-//		Login login = new Login("172.20.10.4",5000);
+//		Login login = new Login("localhost",5000); 
+		Login login = new Login("172.20.10.4",5000);
 		login.hashCode();
 
 	}
